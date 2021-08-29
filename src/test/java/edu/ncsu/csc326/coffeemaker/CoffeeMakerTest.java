15f990fe0d890aa1af1f9bb0c41d4cc83b39d90c
nuttapol-kor <nuttapol.kor@ku.th>
2021-08-30 05:38:15 +0700

    update test case

--- src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -18,7 +18,7 @@
  */
 package edu.ncsu.csc326.coffeemaker;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -91,7 +91,137 @@ public class CoffeeMakerTest {
 		recipe4.setAmtSugar("1");
 		recipe4.setPrice("65");
 	}
-	
+
+	/**
+	 * Only three recipes may be added to the Coffee Maker
+	 * When we add fourth receive to the Coffee Maker, adding should not success
+	 * Then return false.
+	 */
+	@Test
+	public void testAddRecipe() {
+		coffeeMaker.addRecipe(recipe1); // 1
+		coffeeMaker.addRecipe(recipe2); // 2
+		coffeeMaker.addRecipe(recipe3); // 3
+		assertFalse(coffeeMaker.addRecipe(recipe4)); // 4
+	}
+
+	/**
+	 * Each recipe name must be unique in the recipe list.
+	 */
+	@Test
+	public void testAddDuplicateRecipe() {
+		boolean attempt1 = coffeeMaker.addRecipe(recipe1);
+		assertTrue(attempt1);
+		boolean attempt2 = coffeeMaker.addRecipe(recipe1);
+		assertFalse(attempt2);
+	}
+
+	/**
+	 * Given valid index to delete recipe in Coffee maker
+	 * The deleted recipe should not be in recipe book
+	 */
+	@Test
+	public void testDeleteRecipe() {
+		coffeeMaker.addRecipe(recipe1);
+		coffeeMaker.addRecipe(recipe2);
+		String s1 = coffeeMaker.deleteRecipe(0);
+		String s2 = coffeeMaker.deleteRecipe(1);
+		assertEquals("Coffee", s1);
+		assertEquals("Mocha", s2);
+		boolean found = false;
+		Recipe[] recipes = coffeeMaker.getRecipes();
+		// if found the deleted recipe in recipe book, found = true
+		int countRecipe = 0;
+		for (Recipe recipe : recipes) {
+			if (recipe != null) {
+				if (recipe.getName().equals(s1) || recipe.getName().equals(s2)) {
+					found = true;
+				}
+				if (recipe.getClass() == Recipe.class) {
+					countRecipe++;
+				}
+			}
+		}
+		assertFalse(found); // deleted recipe must not found in recipe book.
+		assertEquals(0, countRecipe); // if recipe has been deleted, recipe object should be zero in recipe book
+	}
+
+	/**
+	 * Given the empty recipe book
+	 * When try to delete the recipe (call deleteRecipe()) should return null (return null if the recipe does not exist)
+	 */
+	@Test
+	public void testDeleteRecipeWhenRecipeBookIsEmpty() {
+		String s1 = coffeeMaker.deleteRecipe(0);
+		assertNull(s1);
+	}
+
+	/**
+	 * Given one recipe in Coffee Maker
+	 * When try to delete with invalid index target (negative index) should return null (return null if the recipe does not exist)
+	 */
+	@Test
+	public void testDeleteRecipeWithNegativeIndexRecipeToDelete() {
+		coffeeMaker.addRecipe(recipe1);
+		String s1 = coffeeMaker.deleteRecipe(-1);
+		assertNull(s1);
+	}
+
+	/**
+	 * Given one recipe in Coffee Maker
+	 * When try to delete with invalid index target (Out of bound index) should return null (return null if the recipe does not exist)
+	 */
+	@Test
+	public void testDeleteRecipeWithOutOfBoundIndexRecipeToDelete() {
+		coffeeMaker.addRecipe(recipe1);
+		String s1 = coffeeMaker.deleteRecipe(100);
+		assertNull(s1);
+	}
+
+	/**
+	 * User enter the new recipe information (Recipe object). A recipe name may not be changed.
+	 */
+	@Test
+	public void testEditRecipe() {
+		coffeeMaker.addRecipe(recipe1);
+		coffeeMaker.addRecipe(recipe2);
+		// edit recipe2 (Mocha) by to be the same with recipe3 (Latte) except the recipe name.
+		coffeeMaker.editRecipe(1, recipe3);
+		// the recipe that just edited. the recipe name should be "Mocha".
+		assertEquals("Mocha", coffeeMaker.getRecipes()[1].getName());
+	}
+
+	/**
+	 * Given the empty recipe book
+	 * When try to edit the recipe (call editRecipe()) should return null (return null if the recipe does not exist)
+	 */
+	@Test
+	public void testEditRecipeWhenRecipeBookIsEmpty() {
+		String s1 = coffeeMaker.editRecipe(0, recipe1);
+		assertNull(s1);
+	}
+
+	/**
+	 * Given one recipe in Coffee Maker
+	 * When try to edit with invalid index target (negative index) should return null (return null if the recipe does not exist)
+	 */
+	@Test
+	public void testEditRecipeWithNegativeIndexRecipeToEdit() {
+		coffeeMaker.addRecipe(recipe1);
+		String s1 = coffeeMaker.editRecipe(-1, recipe2);
+		assertNull(s1);
+	}
+
+	/**
+	 * Given one recipe in Coffee Maker
+	 * When try to edit with invalid index target (out of bound index) should return null (return null if the recipe does not exist)
+	 */
+	@Test
+	public void testEditRecipeWithOutOfBoundIndexRecipeToEdit() {
+		coffeeMaker.addRecipe(recipe1);
+		String s1 = coffeeMaker.editRecipe(100, recipe2);
+		assertNull(s1);
+	}
 	
 	/**
 	 * Given a coffee maker with the default inventory
@@ -104,6 +234,13 @@ public class CoffeeMakerTest {
 	@Test
 	public void testAddInventory() throws InventoryException {
 		coffeeMaker.addInventory("4","7","0","9");
+		coffeeMaker.addInventory("10", "10", "10", "10");
+		// boundary case
+		coffeeMaker.addInventory("1","0","0","0");
+		coffeeMaker.addInventory("1","0","0","0");
+		coffeeMaker.addInventory("0", "1", "0", "0");
+		coffeeMaker.addInventory("0", "0", "1", "0");
+		coffeeMaker.addInventory("0", "0", "0", "1");
 	}
 	
 	/**
@@ -118,6 +255,33 @@ public class CoffeeMakerTest {
 	@Test(expected = InventoryException.class)
 	public void testAddInventoryException() throws InventoryException {
 		coffeeMaker.addInventory("4", "-1", "asdf", "3");
+		// try to add malformed quantities for each args
+		// start with string
+		coffeeMaker.addInventory("coffee", "Mike", "Sugar", "Chocolate");
+		// negative quantity
+		coffeeMaker.addInventory("-1", "-1", "-1", "-1");
+	}
+
+	/**
+	 * The units of each item in the inventory are displayed (return as string)
+	 * amount of all ingredient default is 15
+	 */
+	@Test
+	public void testCheckInventory() {
+		StringBuffer s1 = new StringBuffer();
+		s1.append("Coffee: ");
+		s1.append("15");
+		s1.append("\n");
+		s1.append("Milk: ");
+		s1.append("15");
+		s1.append("\n");
+		s1.append("Sugar: ");
+		s1.append("15");
+		s1.append("\n");
+		s1.append("Chocolate: ");
+		s1.append("15");
+		s1.append("\n");
+		assertEquals(s1.toString(), coffeeMaker.checkInventory());
 	}
 	
 	/**
@@ -127,9 +291,97 @@ public class CoffeeMakerTest {
 	 * Then we get the correct change back.
 	 */
 	@Test
-	public void testMakeCoffee() {
+	public void testMakeCoffeePaidMoreThanPrice() {
 		coffeeMaker.addRecipe(recipe1);
-		assertEquals(25, coffeeMaker.makeCoffee(0, 75));
+		// inventory: coffee = 15, milk = 15, sugar = 15, chocolate = 15. enough for make coffee
+		assertEquals(25, coffeeMaker.makeCoffee(0, 75)); // 75-50 = 25
 	}
 
+	/**
+	 * Given a coffee maker with one valid recipe
+	 * When we make coffee, selecting the valid recipe and paying equal to
+	 * 		the coffee costs
+	 * Then we will not get the change.
+	 */
+	@Test
+	public void testMakeCoffeePaidEqualPrice() {
+		coffeeMaker.addRecipe(recipe1);
+		// inventory: coffee = 15, milk = 15, sugar = 15, chocolate = 15. enough for make coffee
+		assertEquals(0, coffeeMaker.makeCoffee(0, 50)); // 50-50 = 0
+	}
+
+	/**
+	 * Given a coffee maker with one valid recipe
+	 * When we make coffee, selecting the valid recipe and paying less than
+	 * 		the coffee costs
+	 * Then we get the money back.
+	 */
+	@Test
+	public void testMakeCoffeePaidLessThanPrice() {
+		coffeeMaker.addRecipe(recipe1);
+		// inventory: coffee = 15, milk = 15, sugar = 15, chocolate = 15. enough for make coffee
+		assertEquals(25, coffeeMaker.makeCoffee(0, 25)); // amtPaid (25) < Price (50) cannot purchase, so change = 25.
+	}
+
+	/**
+	 * Make a coffee with invalid selecting (negative integer)
+	 * makeCoffee() should handle for invalid args, so makeCoffee() must return something and not occur an error.
+	 *
+	 */
+	@Test
+	public void testMakeCoffeeWithNegativeIndexRecipeToPurchase() {
+		coffeeMaker.addRecipe(recipe1);
+		int change = coffeeMaker.makeCoffee(-1, 100);
+		assertEquals(100, change);
+	}
+
+	/**
+	 * Make a coffee with invalid selecting (Out of bound index)
+	 * makeCoffee() should handle for invalid args, so makeCoffee() must return something and not occur an error.
+	 *
+	 */
+	@Test
+	public void testMakeCoffeeWithOutOfBoundRecipeToPurchase() {
+		coffeeMaker.addRecipe(recipe1);
+		int change = coffeeMaker.makeCoffee(100, 100);
+		assertEquals(100, change);
+	}
+
+	/**
+	 * Given one recipe that require some ingredient, but inventory not have enough.
+	 * Then we get money back.
+	 */
+	@Test
+	public void testMakeCoffeeWhenInventoryNotEnough() {
+		// default inventory: coffee = 15, milk = 15, sugar = 15, chocolate = 15
+		coffeeMaker.addRecipe(recipe2); // add Mocha
+		// cannot make Mocha because Mocha need chocolate >= 20, but we got chocolate only 15.
+		int change = coffeeMaker.makeCoffee(0, 100);
+		assertEquals(100, change);
+
+	}
+
+	/**
+	 * Given one recipe to Coffee Maker
+	 * When user purchase beverage the ingredient in inventory should decrease with require ingredient in recipe.
+	 */
+	@Test
+	public void testCheckInventoryWhenPurchaseBeverage() {
+		StringBuffer s1 = new StringBuffer();
+		coffeeMaker.addRecipe(recipe1);
+		coffeeMaker.makeCoffee(0, 100);
+		s1.append("Coffee: ");
+		s1.append("12");
+		s1.append("\n");
+		s1.append("Milk: ");
+		s1.append("14");
+		s1.append("\n");
+		s1.append("Sugar: ");
+		s1.append("14");
+		s1.append("\n");
+		s1.append("Chocolate: ");
+		s1.append("15");
+		s1.append("\n");
+		assertEquals(s1.toString(), coffeeMaker.checkInventory());
+	}
 }
